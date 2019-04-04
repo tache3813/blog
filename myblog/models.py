36cbb67a6722118d12d90f35984ebeea41e18d8e
Kei-Tachibana <un.wackeik@gmail.com>
2019-04-04 14:24:31 +0900

    Create models

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+  """記事のカテゴリ"""
+  name = models.CharField('カテゴリ名', max_length=255)
+  created_at = models.DateTimeField('作成日', default=timezone.now)
+
+  def __str__(self):
+    return self.name
+
+class Post(models.Model):
+  """blogの記事"""
+  title = models.CharField('タイトル', max_length=255)
+  text = models.TextField('本文')
+  created_at = models.DateTimeField('作成日', default=timezone.now)
+  category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+  #author = models.CharField('User.author', max_length=20)
+
+  def __str__(self):
+    return self.title
