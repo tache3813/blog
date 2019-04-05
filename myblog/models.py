96a0fcd7a48d0c0e53272a9ece8103e95ef90160
Kei-Tachibana <un.wackeik@gmail.com>
2019-04-05 18:59:23 +0900

    Language Change Ja => En

--- myblog/models.py
+++ myblog/models.py
@@ -1,32 +1,34 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Category(models.Model):
   """記事のカテゴリ"""
-  name = models.CharField('カテゴリ名', max_length=255)
-  created_at = models.DateTimeField('作成日', default=timezone.now)
+  name = models.CharField('CategoryTag', max_length=255)
+  created_at = models.DateTimeField('CreatedAt', default=timezone.now)
 
   def __str__(self):
     return self.name
 
+
 class Post(models.Model):
   """blogの記事"""
-  title = models.CharField('タイトル', max_length=255)
-  text = models.TextField('本文')
-  created_at = models.DateTimeField('作成日', default=timezone.now)
-  category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+  title = models.CharField('PostTitle', max_length=255)
+  text = models.TextField('PostContent')
+  created_at = models.DateTimeField('PublishedAt', default=timezone.now)
+  category = models.ForeignKey(Category, verbose_name='PostCategory', on_delete=models.PROTECT)
   #author = models.CharField('User.author', max_length=20)
 
   def __str__(self):
     return self.title
 
+
 class Comment(models.Model):
   """記事のコメント"""
-  name = models.CharField('お名前', max_length=30, default="名無し")
-  text = models.TextField('本文')
-  post = models.ForeignKey(Post, verbose_name="紐づく記事", on_delete=models.PROTECT)
-  created_at = models.DateTimeField('投稿日', default=timezone.now)
-
+  name = models.CharField('Your name', max_length=30, default="No name")
+  text = models.TextField('Comment')
+  post = models.ForeignKey(Post, verbose_name="", on_delete=models.PROTECT)
+  created_at = models.DateTimeField('PostedAt', default=timezone.now)
 
   def __str__(self):
     return self.text[:10] #10文字目までを表示する
