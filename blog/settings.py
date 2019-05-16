30d1e375ae682b9d9265a31ad395f5ab579a61fe
Kei-Tachibana <un.wackeik@gmail.com>
2019-05-16 18:34:11 +0900

    settign change

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bz$*)ova*8n91n^413vj*(7u#qu^&l=ia4v54dqyh8uecv$0v3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h'.herokuapp.com']
 
 
 # Application definition
